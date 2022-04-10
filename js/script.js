24285fa8d2d81119aa316dea5fa1e869b6291ae8
Dennis Garbis <dennisgarbis107@gmail.com>
2022-04-10 09:32:09 -0500

    fixed backgroud

--- js/script.js
+++ js/script.js
@@ -22,38 +22,38 @@ jQuery(document).ready(function($) {
 });
 
 Fancybox.bind("[data-fancybox-plyr]", {
-    on: {
-      reveal: (fancybox, slide) => {
-        if (typeof Plyr === undefined) {
-          return;
-        }
-  
-        let $el;
-  
-        if (slide.type === "html5video") {
-          $el = slide.$content.querySelector("video");
-        } else if (slide.type === "video") {
-          $el = document.createElement("div");
-          $el.classList.add("plyr__video-embed");
-  
-          $el.appendChild(slide.$iframe);
-  
-          slide.$content.appendChild($el);
-        }
-  
-        if ($el) {
-          slide.player = new Plyr($el);
-        }
-      },
-      "Carousel.unselectSlide": (fancybox, carousel, slide) => {
-        if (slide.player) {
-          slide.player.pause();
-        }
-      },
-      "Carousel.selectSlide": (fancybox, carousel, slide) => {
-        if (slide.player) {
-          slide.player.play();
-        }
-      },
+  on: {
+    reveal: (fancybox, slide) => {
+      if (typeof Plyr === undefined) {
+        return;
+      }
+
+      let $el;
+
+      if (slide.type === "html5video") {
+        $el = slide.$content.querySelector("video");
+      } else if (slide.type === "video") {
+        $el = document.createElement("div");
+        $el.classList.add("plyr__video-embed");
+
+        $el.appendChild(slide.$iframe);
+
+        slide.$content.appendChild($el);
+      }
+
+      if ($el) {
+        slide.player = new Plyr($el);
+      }
     },
-  });
+    "Carousel.unselectSlide": (fancybox, carousel, slide) => {
+      if (slide.player) {
+        slide.player.pause();
+      }
+    },
+    "Carousel.selectSlide": (fancybox, carousel, slide) => {
+      if (slide.player) {
+        slide.player.play();
+      }
+    },
+  },
+});
